4a45e36afd18cb2a224a560863f73c90316ecc72
phevabb <phevab1@gmail.com>
2026-06-02 00:16:39 +0000

    lint upudate

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,21 @@
+// .eslintrc.js
 module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/vue2-recommended",
+  ],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@babel/eslint-parser",
+    requireConfigFile: false,
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
   rules: {},
 }
